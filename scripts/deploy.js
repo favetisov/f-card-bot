7dc5cce379cb805ee95b021ba7b60c678aa90a0b
Fedor <fedor@Tonys-MBP.lan>
2020-07-31 11:19:00 +0300

    trying to specify filename instead of function name for deploy command

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,3 @@
 const execSync = require('child_process').execSync;
 
-execSync(`cd ${__dirname}/../dist && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env.region} --project=${process.env.project_id}`, { stdio: [0, 1, 2] });
+execSync(`cd ${__dirname}/../dist && gcloud functions deploy ping.js --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env.region} --project=${process.env.project_id}`, { stdio: [0, 1, 2] });
