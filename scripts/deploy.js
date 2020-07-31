a65f8bc85419f7cd0a24874461458a6c303e54c3
Fedor <fedor@Tonys-MBP.lan>
2020-07-31 10:51:02 +0300

    fixed cwd path in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,3 @@
 const execSync = require('child_process').execSync;
 
-execSync(`cd ./dist/lambdas && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env}`, { stdio: [0, 1, 2] });
+execSync(`cd ${__dirname}/../dist/lambdas && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env}`, { stdio: [0, 1, 2] });
