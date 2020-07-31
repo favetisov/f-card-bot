b8898c5dad71f55067a962cb76beab08e287fc54
Fedor <fedor@Tonys-MBP.lan>
2020-07-31 10:55:22 +0300

    logging directory contents

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,5 @@
 const execSync = require('child_process').execSync;
 
-execSync(`cd ${__dirname}/../dist/lambdas && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env}`, { stdio: [0, 1, 2] });
+// execSync(`cd ${__dirname}/../dist/lambdas && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env}`, { stdio: [0, 1, 2] });
+
+execSync(`ls -l ${__dirname}/../`, { stdio: [0, 1, 2] });
