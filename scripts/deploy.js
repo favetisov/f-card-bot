873b37dfe83dba37de8597604854cd03855c7fc3
Fedor <fedor@Tonys-MBP.lan>
2020-07-31 11:06:50 +0300

    deploying ping function from correct path

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,3 @@
 const execSync = require('child_process').execSync;
 
-// execSync(`cd ${__dirname}/../dist/lambdas && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env}`, { stdio: [0, 1, 2] });
-
-execSync(`ls -l ${__dirname}/../dist`, { stdio: [0, 1, 2] });
+execSync(`cd ${__dirname}/../dist && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env}`, { stdio: [0, 1, 2] });
