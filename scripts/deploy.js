b47cda950713ca9fd1b5cfbde340a6ed7c94faf7
Fedor <fedor@Tonys-MBP.lan>
2020-07-31 10:59:49 +0300

    output dist internals

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,4 +2,4 @@ const execSync = require('child_process').execSync;
 
 // execSync(`cd ${__dirname}/../dist/lambdas && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env}`, { stdio: [0, 1, 2] });
 
-execSync(`ls -l ${__dirname}/../`, { stdio: [0, 1, 2] });
+execSync(`ls -l ${__dirname}/../dist`, { stdio: [0, 1, 2] });
