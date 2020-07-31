f5af26781edcee3af191a60236c472735b7e91f8
Fedor <fedor@Tonys-MBP.lan>
2020-07-31 11:09:17 +0300

    fixed  region params in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,3 @@
 const execSync = require('child_process').execSync;
 
-execSync(`cd ${__dirname}/../dist && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env}`, { stdio: [0, 1, 2] });
+execSync(`cd ${__dirname}/../dist && gcloud functions deploy ping --runtime nodejs12 --trigger-http --allow-unauthenticated --region=${process.env.region}`, { stdio: [0, 1, 2] });
