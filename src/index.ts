4f7e15b86f118257ac63ffb5d80be1f93775e592
favetisov <f.avetisov@gmail.com>
2020-08-18 19:36:23 +0300

    added debug response if no answer provided

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import { pingMiddleware } from './middleware/ping.middleware';
 import { WebhookRequest } from './models/webhook-request';
 import { Middleware } from './models/middleware';
-import { noResponseMiddleware } from './middleware/no-response.middleware';
+// import { noResponseMiddleware } from './middleware/no-response.middleware';
 import { useCategoryMiddleware } from './middleware/use-category.middleware';
 
-const middlewareChain: Middleware[] = [pingMiddleware, useCategoryMiddleware, noResponseMiddleware];
+const middlewareChain: Middleware[] = [pingMiddleware, useCategoryMiddleware];
 
 export const onmessage = async (req, res) => {
   const request = new WebhookRequest(req.body.message);
   for (let run of middlewareChain) {
     await run(request);
+  }
+
+  /** remove this after deployment is completed */
+  if (!request.answer) {
+    console.log('body', JSON.stringify(req.body));
+    console.log('from', JSON.stringify(req.body.from));
+    console.log('message', JSON.stringify(req.body.message));
+    request.answer = {
+      method: 'sendMessage',
+      text: JSON.stringify(req.body),
+      chat_id: req.body?.from?.chat?.id,
+    };
   }
 
   if (!request.answer) {
