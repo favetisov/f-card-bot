7e98396cc778deee612a4af332e99caf9ac6d119
fedor <f.avetisov@gmail.com>
2020-09-21 10:53:33 +0300

    removed uses.db file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   // botToken: 'TG_BOT_TOKEN',
-  botToken: '1382487883:AAHw1-Bj3R9I6oDp2bV1g1ANTike5LzNoyE'
+  botToken: '1382487883:AAHw1-Bj3R9I6oDp2bV1g1ANTike5LzNoyE',
 };
